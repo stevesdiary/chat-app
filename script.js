bf8e9a2c84606b4981558b754c40e5c2d5b47098
Stephen Oyeyemi <94493468+stevesdiary@users.noreply.github.com>
2022-09-05 14:24:34 +0100

    New Commit

--- script.js
+++ script.js
@@ -1,23 +1,23 @@
-var socket = io();
-var messages = document.getElementById("messages");
-const messageContainer = document.getElementById("message-container");   
-var form = document.getElementById('form');
-var input = document.getElementById('input');
+// var socket = io();
+// var messages = document.getElementById("messages");
+// const messageContainer = document.getElementById("message-container");   
+// var form = document.getElementById('form');
+// var input = document.getElementById('input');
 
-form.addEventListener('submit', function(e) {
-console.log(input.value);
-   e.preventDefault();
-   if (input.value) {
-   socket.emit('chatmessage', input.value);
-   input.value = '';
-   }
-});
+// form.addEventListener('submit', function(e) {
+// console.log(input.value);
+//    e.preventDefault();
+//    if (input.value) {
+//    socket.emit('chatmessage', input.value);
+//    input.value = '';
+//    }
+// });
 
 
-// To list our chat messages
-socket.on('chatmessage', function(msg) {
-   var item = document.createElement('li');
-   item.textContent = msg;
-   messages.appendChild(item);
-   window.scrollTo(0, document.body.scrollHeight);
-});
+// // To list our chat messages
+// socket.on('chatmessage', function(msg) {
+//    var item = document.createElement('li');
+//    item.textContent = msg;
+//    messages.appendChild(item);
+//    window.scrollTo(0, document.body.scrollHeight);
+// });
